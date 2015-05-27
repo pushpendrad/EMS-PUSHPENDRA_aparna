c41b4a516c461c65b899ce8f85ddac55e37751c2
PUSHPENDRA DEWANGAN_TL <pushpendrad@cybage.com>
2015-05-27 15:43:59 +0530

    PUSP-1

---
+++ src/test/java/com/sample/mvc/seleniumTests/EmployeeDeleteSeleniumTest.java
@@ -0,0 +1,41 @@
+package com.sample.mvc.seleniumTests;
+
+import java.util.concurrent.TimeUnit;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class EmployeeDeleteSeleniumTest {
+  private WebDriver driver;
+  private String baseUrl;
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+       driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testEdit() throws Exception {
+	  driver.get("http://localhost:5050/EmployeeApplicationSprint4-1.0-SNAPSHOT/");
+		
+		driver.findElement(By.linkText("List Employees")).click();
+	
+		driver.findElement(By.xpath("(//a[contains(text(),'delete')])[5]")).click();
+		
+		driver.findElement(By.linkText("Back to Main")).click();
+		
+   
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+   
+  }
+
+ 
+}
+ 
